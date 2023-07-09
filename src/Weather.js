521d8b3cbe6fc7b6ebb8e920dc360dd31869301a
Hayesi <122321074+Hayesi@users.noreply.github.com>
2023-07-09 19:21:51 +0100

    Updated src files

--- src/Weather.js
+++ src/Weather.js
@@ -1,110 +1,107 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
-import WeatherIcon from "./WeatherIcon";
-import CurrentLocation from "./CurrentLocation";
-import Search from "./Search";
-import DateUtil from "./DateUtil";
-import Api from "./Api";
-import Forecast from "./Forecast";
 
-export default class Weather extends Component {
-  static propTypes = {
-    city: PropTypes.string.isRequired,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  state = {
-    city: this.props.city,
-  };
-
-  componentWillMount() {
-    this.refresh(this.state.city);
-  }
-
-  refreshWeatherFromParams(params) {
-    let url = `${Api.url}/data/2.5/weather?appid=${Api.key}&units=metric&${params}`;
-    axios.get(url).then((response) => {
-      this.setState({
-        city: response.data.name,
-        weather: {
-          description: response.data.weather[0].main,
-          icon: response.data.weather[0].icon,
-          precipitation: Math.round(response.data.main.humidity) + "%",
-          temperature: Math.round(response.data.main.temp),
-          time: new DateUtil(new Date(response.data.dt * 1000)).dayTime(),
-          wind: Math.round(response.data.wind.speed) + "km/h",
-        },
-      });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      wind: response.data.wind.speed,
+      city: response.data.city,
     });
   }
 
-  refreshWeatherFromLatitudeAndLongitude = (latitude, longitude) => {
-    this.refreshWeatherFromParams(`lat=${latitude}&lon=${longitude}`);
-  };
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-  refresh = (city) => {
-    this.refreshWeatherFromParams(`q=${city}`);
-  };
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-  render() {
-    if (this.state.weather) {
-      return (
-        <div
-          className="weather-container"
-          style={{ backgroundImage: `url('background.jpg')` }}
+  function search() {
+    const apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <div className="clearfix">
-            <Search refresh={this.refresh} />
-            <CurrentLocation
-              refresh={this.refreshWeatherFromLatitudeAndLongitude}
-            />
-          </div>
-
-          <div className="weather-summary">
-            <div className="weather-summary-header">
-              <h1>{this.state.city}</h1>
-              <div className="weather-detail__text">
-                {this.state.weather.time}
-              </div>
-              <div className="weather-detail__text">
-                {this.state.weather.description}
-              </div>
+          <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
+        </a>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9 ">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control search-input"
+                onChange={handleCityChange}
+              />
             </div>
-
-            <div className="row">
-              <div className="col-sm-6">
-                <div className="clearfix">
-                  <div className="float-left weather-icon">
-                    <WeatherIcon iconName={this.state.weather.icon} />
-                  </div>
-                  <div className="weather-temp weather-temp--today">
-                    {this.state.weather.temperature}
-                  </div>
-                  <div className="weather-unit__text weather-unit__text--today">
-                    °C
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6">
-                <div className="weather-detail__text">
-                  Precipitation: {this.state.weather.precipitation}
-                </div>
-                <div className="weather-detail__text">
-                  Wind: {this.state.weather.wind}
-                </div>
-              </div>
+            <div className="col-3 p-0">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-          <Forecast city={this.state.city} />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          App is loading, <em>please wait...</em>
-        </div>
-      );
-    }
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast
+          coordinates={weatherData.coordinates}
+          city={weatherData.city}
+        />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SheCodes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/shecodesio/weather"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://shecodes-weather.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
   }
 }
