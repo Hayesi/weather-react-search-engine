afc8702101e61218852bd7d5164abdf44fe4518d
Hayesi <122321074+Hayesi@users.noreply.github.com>
2023-07-16 18:45:58 +0100

    Updated api

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    const apiKey = "2daf65f0cdaa917f11026e8a128ce271";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
